03fea105924d9b2f21a385da2987b283507ba4a2
Superherocode <213042559+NguyenDung0101@users.noreply.github.com>
2025-12-30 14:42:25 +0700

    video 31

---
+++ src/chapter04/video31/video31.js
@@ -0,0 +1,17 @@
+console.log("Video 31");
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(5, 6)); // 11
+
+
+// (function () {
+//   console.log("Say hi chạy ngay đi");
+// })();
+
